c7bf3e18456d6408980c2cd39802968ec2358a98
alex <vims@foxmail.com>
2018-09-23 09:27:00 +0800

    change database 1

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))  # 当前文件的绝对路径
 
-HOST = "47.93.236.82"
+HOST = "127.0.0.1"
 PORT = "3306"
 DB = "dcwebdb"
 USER = "root"
-PASS = "hubiao"
+PASS = "Cc!12345"
 CHARSET = "utf8"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset={}".format(USER, PASS, HOST, PORT, DB, CHARSET)
 SQLALCHEMY_DATABASE_URI = DB_URI
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-
+#!usr/bin/python
+# -*- coding: utf-8 -*-
 from app import app
-app.run(debug = True)
+if __name__ == "__main__":
+  app.run()
