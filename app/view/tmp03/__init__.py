3fb60dad60a8a0aedf07284defbcd3692f4d9d8f
huchao <vims@foxmail.com>
2018-09-26 09:50:43 +0800

    增加tmp03的init.py

--- app/view/tmp03/__init__.py
+++ app/view/tmp03/__init__.py
@@ -1,8 +1,8 @@
-#!usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python 
+# _*_ coding: utf-8 _*_
 
 from flask import Blueprint
 
-tmp03= Blueprint('tmp03', __name__)
+tmp03 = Blueprint('tmp03', __name__)
 
 from . import views
