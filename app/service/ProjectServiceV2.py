ce0d79995215e985badf89da1cb9c1f30b4a9132
iTibbers <jxn2014@gmail.com>
2019-10-02 01:55:08 +0800

    修改前端部分页面

--- app/service/ProjectServiceV2.py
+++ app/service/ProjectServiceV2.py
@@ -196,7 +196,7 @@ class ProjectService:
         db2.session.commit()
         storefile(cerFile, awards[0].id, 3, 0)
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:修改项目
@@ -281,7 +281,7 @@ class ProjectService:
             picName = storeMainPic(mainPic)
 
         pass
-    """ 
+    """
     @:param:
     @:return:
     @descrition:查询项目(未分页查询)
@@ -293,7 +293,7 @@ class ProjectService:
             projects = Project.query
         return projects
 
-    """ 
+    """
     @:param:(page_index)->页数
             (per_page)->每页数量
             (condition)->查询条件
@@ -309,7 +309,7 @@ class ProjectService:
         return pagination, projects
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:查询已经审核通过的项目
@@ -321,7 +321,8 @@ class ProjectService:
         endTime = kw['endTime']
         type = kw['type']
         major = kw['major']
-        source = kw['source']
+        # source = kw['source']
+        source = 1
 
         query = ProjectStatus.query.filter(ProjectStatus.status == 3,ProjectStatus.delete_flag == 0)
         if startTime and endTime:
@@ -344,7 +345,7 @@ class ProjectService:
         return pagination, pros
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:查询待审核的项目
@@ -354,7 +355,7 @@ class ProjectService:
         pagination = ProjectStatus.query.filter(ProjectStatus.status == 2, ProjectStatus.delete_flag == 0) \
             .order_by(ProjectStatus.checkTime).paginate(page_index, count, error_out=False)
         return pagination,pagination.items
-    """ 
+    """
     @:param:
         updateContent:更新内容
         condition:查询条件
@@ -366,7 +367,7 @@ class ProjectService:
         db2.session.commit()
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:根据学生ID得到，该学生上传的项目
@@ -386,7 +387,7 @@ class ProjectService:
         return pagination, pros
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:根据项目ID更新内容
@@ -394,7 +395,7 @@ class ProjectService:
     def updateProStatusByPid(self,updateContent,pid):
         db2.session.query(ProjectStatus).filter(ProjectStatus.pid == pid).update(updateContent)
         db2.session.commit()
-    """ 
+    """
     @:param:
     @:return:
     @descrition:得到学生已经上传了的项目(管理员只能对这部分项目进行管理)
@@ -404,7 +405,7 @@ class ProjectService:
         projects = pagination.items
         return pagination, projects
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:逻辑删除项目。
@@ -417,7 +418,7 @@ class ProjectService:
 
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:撤销项目
@@ -430,7 +431,7 @@ class ProjectService:
         }
         self.updateProStatusByPid(updateContent, pid)
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:撤销项目
@@ -442,7 +443,7 @@ class ProjectService:
         }
         self.updateProStatusByPid(updateContent, pid)
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:撤销项目
@@ -455,7 +456,7 @@ class ProjectService:
         }
         self.updateProStatusByPid(updateContent, pid)
 
-    """ 
+    """
     @:param:operation=0代表不通过,非0代表通过。
     @:return:
     @descrition:审核项目
@@ -477,7 +478,7 @@ class ProjectService:
             }
         self.updateProStatusByPid(updateContent,pid)
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:根据PID得到项目内容
@@ -507,7 +508,7 @@ class ProjectService:
 
 
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:根据pid得到项目信息
@@ -656,7 +657,7 @@ class ProjectService:
 
         return path
 
-    """ 
+    """
     @:param:
     @:return:
     @descrition:生成获奖信息,并写入excel
