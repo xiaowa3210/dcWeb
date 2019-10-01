3e6418bad3e07692aa5c1b95549ef0a7394d684e
iTibbers <jxn2014@gmail.com>
2019-10-01 20:41:33 +0800

    整理代码结构

--- run.py
+++ run.py
@@ -3,5 +3,9 @@
 from app import app
 from app.utils.TimerTask import TimerTask, task
 
+# 热更新
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
